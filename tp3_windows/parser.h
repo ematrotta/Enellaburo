d0cbc82a3b110f23ec3d93272d309175f565a7a8
ematrotta <98621645+ematrotta@users.noreply.github.com>
2022-06-16 16:45:33 -0300

    Delete tp3_windows directory

--- tp3_windows/parser.h
+++
@@ -1,6 +0,0 @@
-#include "ArrayPassenger_Flight.h"
-#define TAM_BUFFER 100
-
-int parser_PassengerFromText(FILE* pFile , LinkedList* pArrayListPassenger);
-int parser_FlyghtsFromText(FILE* pFile , LinkedList* pArrayListFlyghts);
-int parser_PassengerFromBinary(FILE* pFile , LinkedList* pArrayListPassenger);
